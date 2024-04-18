510805b72a97d7dea6e41382c4ec4b20d9b967f1
Ole M <olesnoen@gmail.com>
2024-04-18 23:00:19 +0200

    modiefies prerender config

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -5,4 +5,4 @@ export const config = {
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = true;
+export const prerender = false;
